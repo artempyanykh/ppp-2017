e00cc6ff0240da32f66b4675c99b864cd14a697c
NAVICONS\apolushkin <polushkinandrew@yandex.ru>
2017-03-05 15:00:38 +0300

    Добавлен импорт данных из csv файла, а также визуализация этих данных

--- task1.R
+++ task1.R
@@ -1,7 +1,5 @@
-for (i in 1:100){
-  d <- c(0)
-}
-
-f<-c(1, 1, 1)
-
-print("ASLAN TOP 1")
+#reading training data table with two column: Date and Value
+trainingData <- read.csv("C:\\Users\\apolushkin\\Desktop\\ÌÃÓ\\ïðàê\\ÏÏÏ\\tasks\\task1\\training.csv", sep = ",")
+#drawing chart of the time series
+plot(trainingData[[1]], trainingData[[2]])
+#simple moving average for the time series
