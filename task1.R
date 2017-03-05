b4808bd5a25cd99b92f44f2cc649a88e1edec616
NAVICONS\apolushkin <polushkinandrew@yandex.ru>
2017-03-05 21:02:09 +0300

    Добавлены скользящие средние SMA, WMA, EMA, выведены на график с временым рядом.

--- task1.R
+++ task1.R
@@ -1,6 +1,32 @@
 #reading training data table with two column: Date and Value
 trainingData <- read.csv("C:\\Users\\apolushkin\\Desktop\\ÌÃÓ\\ïðàê\\ÏÏÏ\\tasks\\task1\\training.csv", sep = ",")
+
 #drawing chart of the time series
 plot(trainingData[[1]], trainingData[[2]])
-#simple moving average for the time series
+
+#moving averages for the time series
+#Connection to the smatr package, which contains SMA creating function
+library(TTR)
+
+#Detection SMA vector with parameter = 20 for time series trainingData
+smaData <- TTR::SMA(x = trainingData$Value, 20)
+#add SMA vector to the chart
+lines(x = trainingData[[1]], y = smaData, col = 'Red')
+
+#detected Linear Weighted Average (WMA) vector
+wmaData <- TTR::WMA(x = trainingData$Value, 20)
+#add WMA vector to the chart
+lines(x = trainingData[[1]], y = wmaData, col = 'Green')
+
+#detected exponential moving average (EMA) vecor
+emaData <- TTR::EMA(x = trainingData$Value, 20)
+#add WMA vector to the chart
+lines(x = trainingData[[1]], y = emaData, col = 'Blue')
+
+#detected trend, season, 
+timeSeries <- ts(trainingData$Value, frequency = 12, start = c(1959, 1)) 
+tsDecomposition <- stl(log(timeSeries), s.window = 7)
+plot(tsDecomposition, col = 'Blue')
+
+
 
