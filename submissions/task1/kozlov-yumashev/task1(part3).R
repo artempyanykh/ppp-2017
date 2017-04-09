5529e4250262dba30aa0814b830f7ef015ad1266
Artem Yumashev <yumashev.avl@gmail.com>
2017-04-09 14:01:53 +0300

    Update task1(part3).R

--- submissions/task1/kozlov-yumashev/task1(part3).R
+++ submissions/task1/kozlov-yumashev/task1(part3).R
@@ -11,15 +11,21 @@ adf.test(dataint)
 acf(x=dataint) 
 pacf(x=dataint) 
 
-Model1 <- Arima(sourcedata$Value, order=c(1,1,0)) 
+Model1 <- Arima(sourcedata$Value, order=c(0,1,4)) 
 Model1forec <- forecast.Arima(Model1, h=60) 
 plot.forecast(Model1forec)  
 R2_Score(Model1forec$mean, testingdata$Value) 
 
-Model2 <- Arima(sourcedata$Value, order=c(0, 1, 5)) 
+Model2 <- Arima(sourcedata$Value, order=c(2, 1, 4)) 
 Model2forec <- forecast.Arima(Model2, h=60) 
 plot.forecast(Model2forec) 
 R2_Score(Model2forec$mean, testingdata$Value) 
 
-AIC(Model1, Model2) 
+Model3 <- Arima(sourcedata$Value, order=c(2, 1, 0)) 
+Model3forec <- forecast.Arima(Model3, h=60) 
+plot.forecast(Model3forec) 
+R2_Score(Model3forec$mean, testingdata$Value) 
+
+AIC(Model1, Model2, Model3) 
+
 
